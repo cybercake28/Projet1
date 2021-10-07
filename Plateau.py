5fec7d831a28278f3a2d7708e20b693d257ab935
YanisHalit <91349255+YanisHalit@users.noreply.github.com>
2021-10-07 10:58:14 +0200

    Add files via upload

--- Plateau.py
+++ Plateau.py
@@ -1,26 +1,46 @@
 import numpy as np
 import random as rnd
 import matplotlib.pyplot as plt
-from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
 ligne = 6
 colonne = 7
 listwin =[]
 
+
+# Monte Carlo play(etat,joueur):
+#     initialiser les récompenses des actions à 0
+#       (elles représentent la moyenne des victoires par action)
+#     Pour i de 1 à N:
+#         choisir une action a au hasard
+#         Tant que la partie n’est pas finie:
+#             jouer les deux joueurs au hasard
+#         mettre à jour la récompense de l’action a en fonction du résultat
+#     Retourner l’action avec la meilleure probabilité de victoire
 class Plateau :
 
     def __init__(self, L, l):
-
         """
         Fonction permettant d'initialiser le plateau du jeu
-
         """
         self.tab = np.zeros((L,l))
         self.l = ligne
         self.c = colonne
+        self.j1 = []
+        self.j2 = []
+
+    def affichePlateau(self):
+        """
+        Fonction permettant avec la bibli matplotlib d'afficher le terrain
+        """
+        plt.matshow(self.tab)
+        plt.show()
 
     def listWinInit(self):
+        """
+        fonction qui stocke dans une liste toute les tuples gagnants possibles sur le plateau
+        """
+
         #ajout des tuples de victoires sur les lignes
         for lcpt in range(0 , self.l):
             for ccpt in range(0, self.c-3):
@@ -43,9 +63,15 @@ class Plateau :
         return listwin
 
     def reset(self):
-        self.tab = np.zeros((L,l))
+        """
+        fonction permettant de reinitialiser toutes les valeurs du tableau a zero
+        """
+        self.tab = np.zeros((self.l,self.c))
 
     def has_won(self):
+        """
+        Fonction permettant de savoir si le jeu actuel a un gagnant ou non
+        """
         listwin = self.listWinInit()
         tmp = 0
         for tup in listwin:
@@ -61,53 +87,89 @@ class Plateau :
         return False
 
     def play(self, c, joueur):
+        """
+        fonction permettant de faire jouer un joueur a la colonne c si c est possible
+        """
         for l in range(ligne-1, -1, -1 ):
             if( self.tab[l,c-1] != 0 ):
                 continue
             else:
                 self.tab[l,c-1] = joueur
                 return
+        if( c+1 < self.c -1 ):
+            self.play(c+1,joueur)
 
     def is_finished(self):
+        """
+        fonction qui parcours le plateau pour savoir si la partie est finie ou non
+        """
         if( self.has_won() ):
-            print("We have a winner !")
             return True
         for l in range(0 , self.l):
             for c in range(0, self.c):
                 if( self.tab[l,c] == 0 ):
                     return False
-        print("Game is over without winner..!")
         return True
 
     def run(self, joueur1, joueur2):
+        """
+        Tant que le jeu n'est pas fini le jeu continue
+        """
         cpt = 0
-        while( not self.is_finished() ):
-            if( cpt % 2 == 0 ):
-                stock = joueur1.play(self,joueur1)
+        while not self.is_finished() :
+            if cpt % 2 == 0 :
+                stock = joueur1.play_joueur(self,joueur1)
                 self.play( stock, joueur1.id )
                 if( self.is_finished() ):
+                    (self.j1).append(joueur1.nb_coup)
+                    (self.j2).append(0) ##############
                     return 1
                 cpt+=1
             else:
-                stock = joueur2.play(self,joueur2)
+                stock = joueur2.play_joueur(self,joueur2)
                 self.play( stock, joueur2.id )
                 if( self.is_finished() ):
+                    (self.j2).append(joueur2.nb_coup)
+                    (self.j1).append(0) ################
                     return -1
                 cpt+=1
 
+    def distribution(self, joueur1, joueur2, nb_parties):
+        for i in range(0, nb_parties):
+            self.run(joueur1, joueur2)
+            self.reset()
+            joueur1.reset_nb_coup()
+            joueur2.reset_nb_coup()
+        print(self.j1)
+        print(self.j2)
+
 
 class Joueur :
-    def __init__(self, x):
-        self.id = x
 
-    def play(self, plateau, joueur):
+    def __init__(self, x):
+        """
+        Fonction permettant d'initialiser un joueur avec pour jeton x
+        """
+        self.id = x
+        self.nb_coup = 0
+
+    def reset_nb_coup(self):
+        self.nb_coup = 0
+
+    def play_joueur(self, plateau, joueur):
+        """
+        Fonction permettant de faire jouer un joueur
+        """
         c = rnd.randint(0, (plateau.c)-1)
+        self.nb_coup+=1
         return c
 
+plateau = Plateau( ligne, colonne )
+joueurRouge = Joueur(1)
+joueurBleu = Joueur(-1)
+plateau.distribution(joueurRouge, joueurBleu, 10)
 
-pl = Plateau( ligne, colonne )
-j1 = Joueur(1)
-j2 = Joueur(-1)
-pl.run(j1,j2)
-print(pl.tab)
+
+
+
 
